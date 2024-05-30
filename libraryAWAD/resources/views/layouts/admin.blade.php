b89ff422cefe401a3ab3143108f09da3258da4ae
Fazrina22 <fazrinaimaan@gmail.com>
2024-05-31 00:59:09 +0800

    bug with all edit.blade for all model

--- libraryAWAD/resources/views/layouts/admin.blade.php
+++ libraryAWAD/resources/views/layouts/admin.blade.php
@@ -4,9 +4,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Kamunting Library</title>
+
     <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png"/>
     <link rel="stylesheet" href="../assets/css/styles.min.css"/>
+
+    <!-- Scripts -->
+{{--    @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
 </head>
 
 <body>
